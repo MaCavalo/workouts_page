7a2a5de1b2470e3a76bb62a61e848b96ac152b31
MaCavalo <110984633+MaCavalo@users.noreply.github.com>
2024-05-04 18:52:15 +0800

    Update const.ts

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -31,7 +31,7 @@ const USE_GOOGLE_ANALYTICS = false;
 const GOOGLE_ANALYTICS_TRACKING_ID = '';
 
 // styling: set to `true` if you want dash-line route
-const USE_DASH_LINE = false;
+const USE_DASH_LINE = true;
 // styling: route line opacity: [0, 1]
 const LINE_OPACITY = 0.6;
 // styling: map height
@@ -44,7 +44,7 @@ const PRIVACY_MODE = false;
 const LIGHTS_ON = true;
 
 // IF you outside China please make sure IS_CHINESE = false
-const IS_CHINESE = true;
+const IS_CHINESE = false;
 const USE_ANIMATION_FOR_GRID = false;
 const CHINESE_INFO_MESSAGE = (yearLength: number, year: string): string =>
   `户外运动 ${yearLength} 年 ` + ( year === 'Total' ? '' : `，地图展示的是 ${year} 年的轨迹`);
